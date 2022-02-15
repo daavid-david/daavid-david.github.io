58a7cf4440b6f6e354aa01a31b929a2e5059be84
daavid-david <mikulasdejvid@gmail.com>
2022-02-15 16:39:58 +0100

    updated icons
    new:
    - icons (skills page/contact page/work page)
    - size of personal logo

--- app.js
+++ app.js
@@ -3,73 +3,30 @@ var i = 0;
 var executed = false;
 const knowledgeBars = document.getElementById("knowledge")
 
-var elem1 = document.getElementById("myBar1");
+var elem = document.getElementById("myBar1");
 var elem2 = document.getElementById("myBar2");
 var elem3 = document.getElementById("myBar3");
 var elem4 = document.getElementById("myBar4");
 
-function move1() {
-     if (i == 0 && !executed) {
-          i = 1;
-          var width = 1;
-          var id = setInterval(frame, 15);
-          function frame() {
-          if (width >= 90) {
-          clearInterval(id);
-          i = 0;
-          } else {
-               width++;
-               elem1.style.width = width + "%";
-          }
-     }
-     }
-};
+elem.style.width = 0;
+elem2.style.width = 0;
+elem3.style.width = 0;
+elem4.style.width = 0;
 
-function move2() {
+function move() {
      if (i == 0 && !executed) {
           i = 1;
           var width = 1;
           var id = setInterval(frame, 30);
           function frame() {
-          if (width >= 60) {
+          if (width >= 100) {
           clearInterval(id);
           i = 0;
           } else {
                width++;
+               elem.style.width = width + "%";
                elem2.style.width = width + "%";
-          }
-     }
-     }
-};
-
-function move3() {
-     if (i == 0 && !executed) {
-          i = 1;
-          var width = 1;
-          var id = setInterval(frame, 30);
-          function frame() {
-          if (width >= 30) {
-          clearInterval(id);
-          i = 0;
-          } else {
-               width++;
                elem3.style.width = width + "%";
-          }
-     }
-     }
-};
-
-function move4() {
-     if (i == 0 && !executed) {
-          i = 1;
-          var width = 1;
-          var id = setInterval(frame, 30);
-          function frame() {
-          if (width >= 98) {
-          clearInterval(id);
-          i = 0;
-          } else {
-               width++;
                elem4.style.width = width + "%";
           }
      }
@@ -77,10 +34,7 @@ function move4() {
      executed = true;
 };
 
-knowledgeBars.addEventListener("mouseover", move1)
-knowledgeBars.addEventListener("mouseover", move2)
-knowledgeBars.addEventListener("mouseover", move3)
-knowledgeBars.addEventListener("mouseover", move4)
+knowledgeBars.addEventListener("mouseover", move)
 
 $(document).ready(function(){
      $('html').animate({scrollTop:0}, 1);
